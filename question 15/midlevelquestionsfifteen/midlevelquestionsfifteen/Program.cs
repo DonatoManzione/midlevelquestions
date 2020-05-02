75ecac2dc273a8569e02179496b45ce3d2ae0ba7
Edda <edda@dms-MacBook-Pro.local>
2020-05-02 10:14:16 -0400

    addition of question 15 sexplanation

--- question 15/midlevelquestionsfifteen/midlevelquestionsfifteen/Program.cs
+++ question 15/midlevelquestionsfifteen/midlevelquestionsfifteen/Program.cs
@@ -6,6 +6,12 @@ namespace midlevelquestionsfifteen
 	
 		public class Program
 		{
+            //Question 15: How to convert a one-dimensional array to a two-dimensional array
+            // Solution: create a method that takes in the length of each dimension as well as the array itself
+            // Make a loop through the first dimension
+            // for each array in the first dimension, loop through the ints and put them in a one-dimensional array
+            // print each such element
+
 			public static void Main(string[] args)   //main method
 			{
 				int[] OneDArray = new int[] { 27, 45, 78, 90, 63, 81 };
