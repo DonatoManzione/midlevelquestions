8523ee416561ca0746cd2af44a5af477d547b348
Edda <edda@dms-MacBook-Pro.local>
2020-05-02 20:36:04 -0400

    submit question 3

--- question 3/midlevelquestionthree/midlevelquestionthree/Program.cs
+++ question 3/midlevelquestionthree/midlevelquestionthree/Program.cs
@@ -6,7 +6,20 @@ namespace midlevelquestionthree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ReverseOrderOfWords("Let us reverse the word order in this string"));
+        }
+
+        static string ReverseOrderOfWords(string words)
+        {
+            string[] array = words.Split(" ");
+            Array.Reverse(array);
+            string result = "";
+            for(int i = 0; i < array.Length; i++)
+            {
+                result += array[i] + " ";
+            }
+            return result;
+
         }
     }
 }
