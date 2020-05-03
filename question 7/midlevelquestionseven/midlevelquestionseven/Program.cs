b8b141260219df09f2f3f66d8889708fe3f054ec
Edda <edda@dms-MacBook-Pro.local>
2020-05-03 07:55:16 -0400

    question 7 added

---
+++ question 7/midlevelquestionseven/midlevelquestionseven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace midlevelquestionseven
+{
+    class Program
+    {
+        //Question seven: how many substrings in a given string
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManySubstrings("Hello World!"));
+        }
+
+        static int HowManySubstrings(string str)
+        {
+            int numberOfSubStrings = 0;
+            for(int i = 1; i <= str.Length; i++)
+            {
+                numberOfSubStrings += i;
+            }
+            return numberOfSubStrings;
+        }
+    }
+}
