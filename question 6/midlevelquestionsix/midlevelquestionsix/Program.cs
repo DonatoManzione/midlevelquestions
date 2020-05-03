b9a422c366e404bedf15f578e414e12eff7403ac
Edda <edda@dms-MacBook-Pro.local>
2020-05-02 21:20:01 -0400

    add question 6 real

---
+++ question 6/midlevelquestionsix/midlevelquestionsix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace midlevelquestionsix
+{
+    class Program
+    {
+        //remove duplicate characters from a string
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicates("Hello World!"));
+        }
+
+
+        static string RemoveDuplicates(string str)
+        {
+            char[] charactersInString = str.ToCharArray();
+            ISet<char> uniqCharacters = new HashSet<char>(charactersInString);
+            string result = "";
+            foreach(char c in uniqCharacters)
+            {
+                result += c;
+            }
+            return result;
+        }
+    }
+}
