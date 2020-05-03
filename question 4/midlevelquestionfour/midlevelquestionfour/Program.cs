af043825d5cfac63eefd8dc5d18269c268f4fccc
Edda <edda@dms-MacBook-Pro.local>
2020-05-02 20:46:54 -0400

    submit question 4

---
+++ question 4/midlevelquestionfour/midlevelquestionfour/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace midlevelquestionfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseEachWord("Hello World!"));
+        }
+
+
+        public static string ReverseEachWord(string str)
+        {
+            string[] arrayOfWords = str.Split(" ");
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                arrayOfWords[i] = ReverseWord(arrayOfWords[i]);
+            }
+            string result = "";
+            foreach(string word in arrayOfWords)
+            {
+                result += word + " ";
+            }
+            return result;
+
+        }
+
+        public static string ReverseWord(string word)
+        {
+
+            string newWord = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                newWord += word[word.Length - i - 1];
+            }
+            return newWord;
+        }
+    }
+}
