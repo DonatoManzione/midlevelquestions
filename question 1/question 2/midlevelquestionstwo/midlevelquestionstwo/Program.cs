41bd03e33044deaa656322af2950b4b64e83bf6d
Edda <edda@dms-MacBook-Pro.local>
2020-05-02 20:29:34 -0400

    question 2 submission

---
+++ question 1/question 2/midlevelquestionstwo/midlevelquestionstwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace midlevelquestionstwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("Hello World!"));
+            Console.WriteLine(IsPalindrome("hellolleh"));
+        }
+
+
+        static bool IsPalindrome(string str)
+        {
+            int halfLength = str.Length / 2;
+            for(int i = 0; i < halfLength; i++)
+            {
+                if (str[i] == str[str.Length - i - 1])
+                {
+                    continue;
+                }else
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ question 3/midlevelquestionthree/midlevelquestionthree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace midlevelquestionthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
