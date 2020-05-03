d46b42e6dce32495f311ae30809dc949019230b6
Edda <edda@dms-MacBook-Pro.local>
2020-05-03 08:59:00 -0400

    submission of question 16

---
+++ question 16/midlevelquestionsixteen/midlevelquestionsixteen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace midlevelquestionsixteen
+{
+    class Program
+    {
+        // How to find the angle between hour and minute hands of a clock at any given time?
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindAngle(6,15));
+            Console.WriteLine(FindAngle(10, 15));
+        }
+
+     static double FindAngle(int hourFrom12, int minutes)
+        {
+            double oneHourAngle = 30;//degrees per hour for hour hand
+
+            double minuteHandAsHour = minutes / 5;//imagine the minute hand also points to the hour
+
+            double separation = hourFrom12 - minuteHandAsHour;
+
+            if(separation > 6)
+            {
+                separation = 12 - separation;
+            }
+            return separation * 30;
+        }
+    }
+}
