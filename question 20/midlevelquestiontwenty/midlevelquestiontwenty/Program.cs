335b0fb82ec31df476588f61a3d699958cdb2240
Edda <edda@dms-MacBook-Pro.local>
2020-05-05 19:51:27 -0400

    submission of question 20

---
+++ question 20/midlevelquestiontwenty/midlevelquestiontwenty/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace midlevelquestiontwenty
+{
+    class Program
+    {
+        static int x = 9;
+        static int y = 8;
+        //Q20. How to swap two and three numbers?
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variable x holds " + x + " and variable y holds " + y);
+            Swap();
+            Console.WriteLine("Variable x holds " + x + " and variable y holds " + y);
+        }
+
+        static void Swap()
+        {
+            int temp = y;
+            y = x;
+            x = temp;
+        }
+    }
+}
