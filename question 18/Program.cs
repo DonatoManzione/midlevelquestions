2e08a238726da3fbfdea901463a90ab0839ac318
Edda <edda@dms-MacBook-Pro.local>
2020-05-05 16:40:35 -0400

    midlevel question nineteen and part of eighteen

---
+++ question 18/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace midlevelquestioneighteen
+{
+    class Program
+    {
+        //Q18. How to generate Fibonacci series using Iterative Approach, Recursive Method, by using Array? 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ question 19/midlevelquestionnineteen/midlevelquestionnineteen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace midlevelquestionnineteen
+{
+    class Program
+    {
+        //Q19. How to find prime numbers between 1 to 100 using for and while loop?
+        static void Main(string[] args)
+        {
+            Console.Write("2, 3, 5, 7, ");
+            for(uint i = 11; i <= 100; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+
+
+        static bool IsPrime(uint num)
+        {
+            for(uint i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+
+        
+    }
+}
